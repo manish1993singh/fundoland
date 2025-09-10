d6028dad8eb5ecf98b7e4924fcde013db60b3d1b
Manish Kumar <manish1993singh@gmail.com>
2025-09-10 23:31:13 +0530

    redis cache enabled

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -1,9 +1,12 @@
 package com.example.user;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // Enable caching for the application
 public class UserApplication {
 
 	public static void main(String[] args) {
--- user/src/main/java/com/example/user/entity/User.java
+++ user/src/main/java/com/example/user/entity/User.java
@@ -1,5 +1,7 @@
 package com.example.user.entity;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,7 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
-public class User {
+public class User implements Serializable{
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
   private Integer id;
